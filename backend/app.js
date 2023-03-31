b733f269294a611e220e8d0f5fc25dab0f571b18
MOHIT RANA <awesomemohit20@gmail.com>
2023-03-31 23:43:04 +0530

    Connected with database and started the server

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,18 @@
 const express = require("express")
 const app = express();
+const connectDB = require("./connect");
 
 app.get("/", (req,res) => {
     res.send("Hello world")
 })
 
-app.listen("3000", () => {
-    console.log("the server is running on: http://localhost:3000")
-})
+try {
+    async function start() {
+      const { Models } = await connectDB();
+      app.listen(3000, console.log("server listening on port:3000"));
+    }
+    start();
+  } catch (error) {
+        console.error("Sorry! Cannot start the server!", error);
+  }
+  
