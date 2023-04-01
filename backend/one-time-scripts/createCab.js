27eefb135fa2167922f1e223ec7352ae97a39c31
MOHIT RANA <awesomemohit20@gmail.com>
2023-04-01 19:17:23 +0530

    script for adding instances of cab's

---
+++ backend/one-time-scripts/createCab.js
@@ -0,0 +1,19 @@
+const connectDb = require("../connect");
+
+async function start(){
+    const { Models } = await connectDb();
+    const { CabList } = Models;
+    const cabInstance = new CabList({
+        cabId: 1,
+        sourceLoc: "A",
+        price: 15,
+        cabStatus: false
+    });
+
+    console.log(cabInstance);
+    await cabInstance.save();
+    console.log({ message: "Successfully created a cab instance"});
+    process.exit(0);
+}
+
+start();c
