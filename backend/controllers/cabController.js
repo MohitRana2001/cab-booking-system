a2487052133e179be10d68e2f2077a9e53bd0c5b
MOHIT RANA <awesomemohit20@gmail.com>
2023-04-01 21:39:54 +0530

    api for getting all the cab's details

---
+++ backend/controllers/cabController.js
@@ -0,0 +1,18 @@
+function cabController(app, Models){
+    const { CabList } = Models;
+    app.get("/cabs", async (req,res) => {
+        try {
+            const cabInstances = await CabList.find({});
+            if(cabInstances){
+                return res.status(200).json({ cabInstances });
+            }else{
+                return res.status(404).json({ message: "No records found!"});
+            }
+        } catch (error) {
+            return res.status(500).json({ message: error });
+        }
+    })
+}
+
+
+module.exports = cabController;
